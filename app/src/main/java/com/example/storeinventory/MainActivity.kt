aa477d4c3252bfa79d7fda9ffdae1a442f25fa33
Esteban Sánchez <essoler@gmail.com>
2022-01-25 19:29:10 +0100

    Remove unused drawerlayout

--- app/src/main/java/com/example/storeinventory/MainActivity.kt
+++ app/src/main/java/com/example/storeinventory/MainActivity.kt
@@ -1,9 +1,8 @@
 package com.example.storeinventory
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
@@ -11,7 +10,6 @@ import com.example.storeinventory.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController
-    private lateinit var drawerLayout: DrawerLayout
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,7 +18,6 @@ class MainActivity : AppCompatActivity() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         setSupportActionBar(binding.toolbar)
-        drawerLayout = binding.mainActivityDrawerLayout
         navController = this.findNavController(R.id.nav_host_fragment)
         NavigationUI.setupActionBarWithNavController(this, navController)
     }
